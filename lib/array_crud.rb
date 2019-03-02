5ea0d901d7500ed02e13146bf07091497068516f
Patrick A <akolo@hotmail.com>
2019-03-02 22:42:50 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,20 +18,17 @@ def add_element_to_start_of_array(array, element)
     array = ["cookies", "chips", "candy", "ice cream"]
     element = "wow" 
     array.unshift element
-    puts array.inspect
   
 end
 
 def remove_element_from_end_of_array(array)
     array = ["cookies", "chips", "candy", "ice cream", "arrays!"]
     array.pop  
-    puts array.inspect
 end
 
 def remove_element_from_start_of_array(array)
     array = ["wow", "cookies", "chips", "candy", "ice cream"]
     array.shift 
-    puts array.inspect
   
 end
 
@@ -45,11 +42,11 @@ end
 def retrieve_first_element_from_array(array)
     array = ["wow", "cookies", "chips", "candy", "ice cream"]
     array.first 
-    puts array.inspect
+    
 end
 
 def retrieve_last_element_from_array(array)
     array = ["cookies", "chips", "candy", "ice cream", "arrays!'"]
     array.last 
-    puts array.inspect
+    
 end
