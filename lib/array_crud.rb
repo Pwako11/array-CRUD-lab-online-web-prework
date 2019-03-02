caf290ceb6912d5e883207f1196b9db9375eef86
Patrick A <akolo@hotmail.com>
2019-03-02 22:36:19 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -43,7 +43,7 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-    array = ["cookies", "chips", "candy", "ice cream"]
+    array = ["wow", "cookies", "chips", "candy", "ice cream"]
     array.first 
     puts array.inspect
 end
