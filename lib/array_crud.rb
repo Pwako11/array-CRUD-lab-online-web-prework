494727d0863e25f8d0fea8f5ca67f2671bbf57b6
Patrick A <akolo@hotmail.com>
2019-03-02 22:35:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -29,7 +29,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-    array = ["cookies", "chips", "candy", "ice cream"]
+    array = ["wow", "cookies", "chips", "candy", "ice cream"]
     array.shift 
     puts array.inspect
   
