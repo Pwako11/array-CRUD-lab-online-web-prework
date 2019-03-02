f87b8addebe1e1a41d7befd7598ad735e898f35c
Patrick A <akolo@hotmail.com>
2019-03-02 22:32:19 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,7 @@ end
 
 def add_element_to_end_of_array(array, element)
     array = ["cookies", "chips", "candy", "ice cream"]
-    element = "pie" 
+    element = "arrays!" 
     array << element
   
 end
